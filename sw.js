fb70910a563f962fdc4094f7d11b2fef423d0d12
WorkBuddy <workbuddy@local>
2026-08-13 15:16:50 +0800

    部署 MLBB 电竞百科静态站点 (33 页面, 子路径 /mlbb-wiki 相对路径版)

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'mlbb-wiki-v1';
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy));
+        return res;
+      })
+      .catch(() => caches.match(req))
+  );
+});
